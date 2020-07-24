e883cf0bd64f54bc0a8b28a259fe948e4f10d5b7
Marc Piechura <marcpiechura@epmap-system.com>
2020-07-24 22:25:35 +0200

    show values

--- api/Endpoint/index.js
+++ api/Endpoint/index.js
@@ -1,7 +1,13 @@
 module.exports = async function (context, req) {
+  const header = req.headers["x-ms-client-principal"];
+  const encoded = Buffer.from(header, "base64");
+  const decoded = encoded.toString("ascii");
+  const customer = context.req.query.customer
+  req.
     context.res = {
       body: {
-        text: "Hello from the API"
+        text: decoded,
+        customer: customer
       }
     };
   };
